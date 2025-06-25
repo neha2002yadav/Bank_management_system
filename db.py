3e4cc36ea4f6de7426b31ff1ebe3d87723753e77
Neha Yadav <nehayadav001.yadav@gmail.com>
2025-06-26 02:08:55 +0530

    Add files via upload

--- db.py
+++ db.py
@@ -1,10 +1,23 @@
+# db.py
+
 import mysql.connector
+from mysql.connector import Error
 
 def create_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="suman613212",
-        database="bank_management_system"
-    )
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Neha@123",
+            database="bank_management_system"
+        )
+        if connection.is_connected():
+            print("✅ Connected to MySQL database.")
+            return connection
+    except Error as e:
+        print(f"❌ Database connection error: {e}")
+        return None
+
+
+
 
